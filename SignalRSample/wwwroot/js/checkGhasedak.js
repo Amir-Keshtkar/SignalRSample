3de3212f023f18f48170847dbea34d907ac3f81e
Amirhossein Keshtkar <amir78ahkl@gmail.com>
2023-01-29 23:19:00 +0330

    add ghasdak testerr

---
+++ SignalRSample/wwwroot/js/checkGhasedak.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../lib/microsoft/signalr/dist/browser/signalr.js" />
+//Create Connection
+
+//var connectioncGhasedak = new signalR.HubConnectionBuilder()
+//    .withUrl("http://localhost:5015/notificationhub").build();
+
+//http://192.168.1.177:8080/Notification/
+var connectioncGhasedak = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:5015/Notification/"
+        , { withCredentials: false }
+    )
+    .build();
+
+
+//start connection
+function fullfilled() {
+    console.log("connection successfully stablished with Ghasedak Hub");
+    /*connectioncGhasedak.send();*/
+
+    connectioncGhasedak.invoke('SeenNotifications', [6]).then((value) => {
+        console.log("seen notif: " + value);
+    });;
+
+    console.log("Vola!");
+
+    //connectioncGhasedak.invoke("LoadNotifications").then((value) => {
+    //    console.log("value is: " + value);
+    //});
+}
+function rejected() {
+    console.log("connection rejected from Ghasedak hub!");
+}
+
+connectioncGhasedak.start().then(fullfilled, rejected);
